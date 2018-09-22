9df82b047ab3768f11266f6daf44f885d97f626e
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-09-22 07:18:55 +0600

    lms design

---
+++ LmsApp/Model/Course.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common;
+
+namespace Model
+{
+   public class Course : Entity
+    {
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+        public string TrainerName { get; set; }
+       // public string TrainerId { get; set; }
+        public double AverageRating { get; set; }
+        public int EnrolledCount { get; set; }
+        public double RegularPrice { get; set; }
+        public double DiscountPrice { get; set; }
+        public bool IsFree { get; set; }
+        public string Topic { get; set; }
+        public int TotalDurationHour { get; set; }
+        public int TotalLectureCount { get; set; }
+        public StudentLavel StudentLavel { get; set; }
+        public string Language { get; set; }
+        public int ViewCount { get; set; }
+
+    }
+
+    public enum StudentLavel
+    {
+        Beginner = 0,
+        Intermediate,
+        Expert  
+
+    }
+}
