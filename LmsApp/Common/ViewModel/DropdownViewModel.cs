9c495912d44f22cd6ad46ed56a07a8d717fa219c
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-09-22 12:13:32 +0600

    base request model and Dropdown

---
+++ LmsApp/Common/ViewModel/DropdownViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.ViewModel
+{
+   public class DropdownViewModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+
+        public object Data { get; set; }
+    }
+}
